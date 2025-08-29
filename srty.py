aabc9bbe3c19b39118b9730cc9eefba29eed8e41
Bharath M <bharath08092002@gmail.com>
2025-08-29 17:00:59 +0530

    Max

---
+++ srty.py
@@ -0,0 +1,12 @@
+import sys
+import math
+k = len(sys.argv)
+maxi = -math.inf
+mini = math.inf
+for i in range(1,k):
+    if int(sys.argv[i])>maxi:
+        maxi = int(sys.argv[i])
+    if(mini>int(sys.argv[i])):
+        mini = int(sys.argv[i])
+print(maxi)
+print(mini)
